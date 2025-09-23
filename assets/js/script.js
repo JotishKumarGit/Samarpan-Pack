e8d5ffcd3af92f0fc4ce88dcda3a2501fe9980fa
Jotish <jotishk649@gmail.com>
2025-09-23 15:25:48 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+// aos animation ke liye initialization
+ AOS.init();
+ console.log("✅ script.js is connected!");
+
+
+//  Send message on whatsapp
+ function sendToWhatsApp() {
+    var name = document.getElementById("name").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var phone = document.getElementById("phone").value.trim();
+    var message = document.getElementById("message").value.trim();
+
+    var whatsappNumber = "919626107779";
+
+    var url = "https://wa.me/" + whatsappNumber +
+      "?text=" + encodeURIComponent(
+        "📩 New Inquiry from Website\n\n" +
+        "👤 Name: " + name + "\n" +
+        "📧 Email: " + email + "\n" +
+        "📱 Phone: " + phone + "\n" +
+        "💬 Message: " + message
+      );
+
+    window.open(url, '_blank');
+    return false; // Prevent form from submitting normally
+  }
+
